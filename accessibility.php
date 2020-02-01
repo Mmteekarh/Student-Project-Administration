4d2f32cede38bf022538957babf79949e4e04dab
Jake Taylor <jakeitaylor@hotmail.com>
2020-02-01 01:22:52 +0000

    Added help and accessibility pages

---
+++ accessibility.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Database connection and title -->
+    <?php include "includes/connect.php" ?>
+    <title>Accessibility</title>
+
+</head>
+
+
+<body>
+
+    <!-- Includes navigation bar -->
+    <?php include "includes/nav.php" ?>
+
+    <!-- Header containing the title and subtitle of the page -->
+    <header>
+
+        <br>
+        <center><h2>Change Contrast</h2></center>
+        <br>
+        
+    </header>
+
+    <!-- Main Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <h4>Change Contrast</h4>
+            <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text </p>
+
+            <br><br>
+
+            <center>
+                <div class="row">
+                    
+                    <button type="submit" class="btn btn-success">High Contrast</button>
+
+                    <br>
+
+                    <button type="submit" class="btn btn-success">Normal Contrast</button>
+                </div>
+            </center>
+
+            <br><br>
+            
+        </div>
+
+        <div class="row">
+            <h4>Accessibility Information</h4>
+            <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text </p>
+
+        </div>
+
+    </div>
+     
+    <!-- Includes footer -->
+    <?php include "includes/footer.php" ?>
+
+</body>
+
+</html>
+
---
+++ help.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Database connection and title -->
+    <?php include "includes/connect.php" ?>
+    <title>Help + FAQ - SPAS</title>
+
+</head>
+
+
+<body>
+
+    <!-- Includes navigation bar -->
+    <?php include "includes/nav.php" ?>
+
+    <!-- Header containing the title and subtitle of the page -->
+    <header>
+
+        <br>
+        <center><h2>FAQ</h2></center>
+        <br>
+        
+    </header>
+
+    <!-- Main Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <h4>Test question?</h4>
+            <p>Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer </p>
+
+            <br><br>
+
+            <h4>Test question?</h4>
+            <p>Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer </p>
+
+            <br><br>
+
+            <h4>Test question?</h4>
+            <p>Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer </p>
+
+            <br><br>
+
+            <h4>Test question?</h4>
+            <p>Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer Test answer </p>
+
+            <br><br>
+            
+        </div>
+
+        <div class="row">
+            <h4>Project Process Information</h4>
+            <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text </p>
+
+        </div>
+
+    </div>
+     
+    <!-- Includes footer -->
+    <?php include "includes/footer.php" ?>
+
+</body>
+
+</html>
+
